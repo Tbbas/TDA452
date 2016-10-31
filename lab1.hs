65c20d44e1f00372c92f2e6b163c5d9f5b6af2f6
jaxing <jaxing@student.chalmers.se>
2016-10-31 15:08:11 +0100

    hej

--- lab1.hs
+++ lab1.hs
@@ -39,5 +39,7 @@ power2 n k
 -- power for odd k should be equal to power1 and power2 for odd k,
   --  seeing as this is where they differ
 
+prop_power n k = (power n k == power1 n k) && (power n k == power2 n k) && (power n k == n^k)
 
-prop_power n k = (power n k == power1 n k) && (power n k == power2 n k)
+
+test_power =(power 0 3 == 0) && (power 3 0 == 1) && (prop_power 4 4) && (prop_power 4 5)
