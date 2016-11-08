bcf56eca287a6b655196feb1479f2dfda1c00d4e
jaxing <jaxing@student.chalmers.se>
2016-11-08 08:35:50 +0100

    Fixed shit

--- Lab2/BlackJack_Jax.hs
+++ Lab2/BlackJack_Jax.hs
@@ -17,10 +17,28 @@ prop_emptyHand = size emptyHand == 0
 -- Test Cases:
 --  Two hands of the same cards but different suits should have the same value
 --  The value of the hand cannot be smaller than the size of the hand
---  The value of the hand cannot be larger than 20 + 11 (draw ace at twenty, aka draw the largest card at twenty)
---  something about numberOfAce
 --  The value of a empty hand should be 0
+handValue :: Hand -> Integer
+handValue Empty = 0
+handValue (Add card hand) = valueCard card + handValue hand
 
+-- Tests handValue
+handHearts :: Hand
+handHearts = Add (Card (Numeric 5) Hearts)
+  (Add (Card Queen Hearts) Empty)
+
+handSpades :: Hand
+handSpades = Add (Card (Numeric 5) Spades)
+  (Add (Card Queen Spades) Empty)
+
+prop_handValue1 :: Bool
+prop_handValue1 =  handValue handHearts == handValue handSpades
+
+prop_handValue2 :: Hand -> Bool
+prop_handValue2 hand = size hand <= handValue hand
+
+
+-- given a rank calculates the value
 valueRank :: Rank -> Integer
 valueRank (Numeric rank) = rank
 valueRank Jack = 10
@@ -28,37 +46,17 @@ valueRank Queen = 10
 valueRank King = 10
 valueRank Ace = 11
 
+-- given a card calculates the value
 valueCard :: Card -> Integer
 valueCard (Card rank _) = valueRank rank
 
+-- given a  hand calualtes the number of aces in it
 numberOfAce :: Hand -> Integer
 numberOfAce Empty = 0
 numberOfAce (Add (Card Ace _) hand) = 1 + numberOfAce hand
 numberOfAce (Add card hand) = numberOfAce hand
 
---
-
-handValue :: Hand -> Integer
-handValue Empty = 0
-handValue (Add card hand) = valueCard card + handValue hand
-
---
-
-handHearts :: Hand
-handHearts = Add (Card (Numeric 5) Hearts)
-                  (Add (Card Queen Hearts) Empty)
-
-handSpades :: Hand
-handSpades = Add (Card (Numeric 5) Spades)
-                  (Add (Card Queen Spades) Empty)
-
-prop_handValue1 :: Bool
-prop_handValue1 =  handValue handHearts == handValue handSpades
-
-prop_handValue2 :: Hand -> Bool
-prop_handValue2 hand = size hand <= handValue hand && handValue hand <= (20 + 11)
-
--- (Given a sorted hand ?)Calculate if a player goes bust
+-- (Given a sorted hand ?)Calculate if a player goes busthand
 -- Test Case:
 --  True if given a bust hand
 --  a bust hand cannot beat a non-bust hand (no other hand?)
