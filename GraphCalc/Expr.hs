b1144385e316ee8e180abf1ede93fc3c83b9d56e
Tbbas <covernant_4@hotmail.com>
2016-12-07 08:55:59 +0100

    Wrote show function

--- GraphCalc/Expr.hs
+++ GraphCalc/Expr.hs
@@ -1,45 +1,28 @@
 data Expr = Num Float
-          | Op Op
-          | Func Func
-
-data Op   = Mul Expr Expr
+          | Mul Expr Expr
           | Add Expr Expr
-
-data Func = Sin Expr
+          | Sin Expr
           | Cos Expr
 
 instance Show Expr where
    show = showExpr
 
-instance Show Op where
-   show = showOp
-
-instance Show Func where
-   show = showFunc
-
 showExpr :: Expr -> String
-showExpr (Num f )= show f
-showExpr x    = show x
-
-showOp :: Op -> String
-showOp (Mul m n) = show m ++ "*" ++ show n
-showOp (Add m n) =
+showExpr (Num f )     = show f
+showExpr (Mul m n)    = show m ++ "*" ++ show n
+showExpr (Add m n)    =
   case m of
-    (Op (Mul m' n')) -> case n of
-      (Op (Mul x y)) -> "(" ++ show m ++ ")+(" ++ show n ++ ")"
-      otherwise -> "(" ++ show m ++ ")+" ++ show n
-    otherwise -> case n of
-      (Op (Mul x y)) -> show m ++ "+(" ++ show n ++ ")"
-      otherwise -> show m ++ "+" ++ show n
-
-showFunc :: Func -> String
-showFunc (Sin x) =
-    case x of
-      (Op (Mul x' y)) -> "Sin(" ++ show x ++ ")"
-      (Op (Add x' y)) -> "Sin(" ++ show x ++ ")"
-      otherwise -> "Sin" ++ show x
-showFunc (Cos x) =
-    case x of
-      (Op (Mul x' y)) -> "Cos(" ++ show x ++ ")"
-      (Op (Add x' y)) -> "Cos(" ++ show x ++ ")"
-      otherwise -> "Cos" ++ show x
+    (Mul _ _) -> case n of
+      (Mul _ _) -> "(" ++ show m ++ ") + (" ++ show n ++ ")"
+      otherwise  -> "(" ++ show m ++ ") +" ++ show n
+    otherwise -> show m ++ "+" ++ show n
+showExpr (Sin x)      =
+  case x of
+    (Mul _ _ ) -> "Sin(" ++ show x ++ ")"
+    (Add _ _ ) ->  "Sin(" ++ show x ++ ")"
+    otherwise -> "Sin" ++ show x
+showExpr (Cos x)      =
+  case x of
+    (Mul _ _ ) -> "Cos(" ++ show x ++ ")"
+    (Add _ _ ) ->  "Cos(" ++ show x ++ ")"
+    otherwise -> "Cos" ++ show x
